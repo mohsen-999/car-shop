2f4d848f0434c0433d9be6da2b4ae10d8a7e7625
mohsen-999 <114609660+mohsen-999@users.noreply.github.com>
2023-08-20 19:06:50 +0330

    Initial commit

---
+++ components/Hero.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Image from "next/image";
+
+import { CustomButton } from "@components";
+
+const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+         where your driving dreams come true!
+        </h1>
+
+        <p className="hero__subtitle">
+        Simplify your car buying experience with our easy booking process.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/herroo.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
